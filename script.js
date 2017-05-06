40789834d3d901a6647b55045523d240ba99d9cb
CNDavidson <cndragn@gmail.com>
2017-05-06 15:05:53 -0400

    Add on-click behavior to change temp F to C

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var html = "https://ipinfo.io/geo";
-var html2 = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&&units=imperial&appid=c852eff24ce63a4936c2501cfe5124fc";
+var html2 = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=c852eff24ce63a4936c2501cfe5124fc";
 
 var getCity = function (data) {
   $("#city").text(data["city"]);
@@ -8,7 +8,20 @@ var getRegion = function (data) {
   $("#region").text(data["region"]);
 };
 var getTemp = function (data) {
-  $("#temp").text(data["main"]["temp"]);
+  var temp = ((data["main"]["temp"] - 273.15) * 9/5 +32).toFixed(0);
+  var temp2 = (data["main"]["temp"] - 273.15).toFixed(0);
+  
+  $("#temp").text(temp + '\xB0 F');
+  
+   $("button").click(function() {
+        if ($('.btn').is(":contains('\xB0 F')")) {
+           $("#temp").text(temp + '\xB0 F');
+          $('.btn').html('View ' + '\xB0 C');
+        } else {
+            $("#temp").text(temp2 + '\xB0 C');
+          $('.btn').html('View ' + '\xB0 F');
+        }
+      });
 };
 var getConditions = function (data) {
   $("#conditions").text(data["weather"][0]["main"]);
@@ -25,18 +38,4 @@ $(document).ready(function () {
   $.getJSON(html2, getTemp, "jsonp");
   $.getJSON(html2, getConditions, "jsonp");
   $.getJSON(html2, getIcon, "jsonp");
-});
-
-//Change temp F to C
-
-var temp2 = ((getTemp - 32) * 5 / 9).toFixed(0);
-
-      $("button").click(function() {
-        if ($('.btn').is(":contains('\xB0 F')")) {
-           $("#temp").text('Fahrenheiet');
-          $('.btn').html('View ' + '\xB0 C');
-        } else {
-           $("#temp").text('Celcius');
-          $('.btn').html('View ' + '\xB0 F');
-        }
-      });
+}); 
